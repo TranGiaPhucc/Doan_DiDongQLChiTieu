8ebf196e4abe211e615386071226d38ef2ecf113
TranGiaPhucc <shiroineko1999@gmail.com>
2022-04-16 14:56:44 +0700

    qlct

---
+++ QuanLyChiTieu/app/src/main/java/com/hufi/quanlychitieu/ChiTiet.java
@@ -0,0 +1,14 @@
+package com.hufi.quanlychitieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChiTiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet);
+    }
+}
---
+++ QuanLyChiTieu/app/src/main/java/com/hufi/quanlychitieu/Login.java
@@ -0,0 +1,60 @@
+package com.hufi.quanlychitieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    Button btnLogin, btnRegister;
+    EditText txtUsername, txtPassword;
+    Database db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+
+
+                if (username.equals("") == true || password.equals("") == true) {
+                    Toast.makeText(Login.this, "Chưa nhập username/password.", Toast.LENGTH_SHORT).show();
+                }/*
+                else if (db.getNguoiDung(username) == {"username", "password"})
+                {
+
+                }
+                */
+                else {
+                    Intent intent = new Intent(Login.this, TrangChu.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+        
+        
+    }
+}
